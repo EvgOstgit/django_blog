b505183337d37dd858a62acacc80a4bdcd2d7e78
evgeniy <git_evg_ost@internet.ru>
2025-05-05 17:02:23 +0300

    creating templates "home", "about", "post_list"

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,26 +1,22 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import HttpResponse
 
 # представление для отображения списка постов
 def post_list(request):
+    # запрашиваем у БД все записи таблицы Post (объекты класса Post)
+    # Post - модель, которая описывает таблицу в БД
+    # objects - это менеджер, который предоставляет методы для работы с записями в БД
+    # .all() - это метод, который возвращает QuerySet (набор записей) со всеми объектами модели Post
+    # ответный набор записей будет отсортирован в соответствии с правилом .order_by (-created_at - от новых записей к старым)
     posts = Post.objects.all().order_by('-created_at')
+
+    # формирование html-страницы на основе шаблона (шаблон находится в templates/blog/)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 # домашняя страница
 def home(request):
-    return HttpResponse("""
-        <h1>Главная страница</h1>
-        <ul>
-            <li><a href="/about/">О нас</a></li>
-            <li><a href="/articles/">Статьи</a></li>
-        </ul>
-    """)
+    return render(request, 'blog/home.html')
 
 # о нас
-def about(requests):
-    return HttpResponse("""
-        <h1>О нас</h1>
-        <p>Это простой блог на Django.</p>
-        <a href="/">На главную</a>
-""")
+def about(request):
+    return render(request, 'blog/about.html')
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -20,6 +20,7 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('posts/', views.post_list, name='post_list'),
 ]
