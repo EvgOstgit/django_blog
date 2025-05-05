556eaac535ba1e9b3f56c51caea259069f98d1e5
evgeniy <git_evg_ost@internet.ru>
2025-05-05 17:12:55 +0300

    creating a category model and changing the post model to link to categories

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.utils import timezone
 
+# модель категории с единственным полем - названием
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
 # модель постов
 class Post(models.Model):
     title = models.CharField(max_length=200) # заголовок поста
     content = models.TextField() # основной текст поста
     created_at = models.DateTimeField(default=timezone.now) # дата создания поста (по умолчанию - текущая)
     updated_at = models.DateTimeField(auto_now=True) # дата последнего обновления (по умолчанию автоматически обновляется при сохранении)
+    categories = models.ManyToManyField(Category, related_name='posts') #related_name='posts' позволяет получить все посты, связанные с категорией
 
     def __str__(self):
         return self.title
